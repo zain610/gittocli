ccf58f8d5e9dcbe97bfbb47b0af113ea06ef68b2
zain610 <znshroff@gmail.com>
2019-02-26 17:39:39 +1100

    Fetching data successful

--- src/index.js
+++ src/index.js
@@ -8,13 +8,38 @@ const { fetchRepositories } = require("./util/Data");
 
 program.version("1.0");
 
+const parseData = rawData => {
+  const { id, name, html_url, watchers: stars } = rawData;
+  console.log(id, name);
+  return {
+    id,
+    name,
+    html_url,
+    stars
+  };
+};
+
 program
   .command("search [keyword]")
   .description("Search for Repository using name")
   .action(keyword => {
-    if (keyword) fetchRepositories(keyword);
+    if (keyword)
+      fetchRepositories(keyword)
+        .then(response => {
+          // do something
+          response.forEach(repo => {
+            let data = parseData(repo);
+            console.log(data);
+          });
+        })
+        .catch(err => console.log(err));
     else {
-      getRepoName().then(response => console.log(response.repoName));
+      getRepoName().then(response => {
+        console.log(response.RepoName);
+        fetchRepositories(response.RepoName)
+          .then(response => {})
+          .catch(err => _);
+      });
     }
   });
 
--- src/util/Data.js
+++ src/util/Data.js
@@ -1,33 +1,16 @@
 const axios = require("axios");
 
-fetchRepositories = keyword => {
+function fetchRepositories(keyword) {
   params = {
     q: keyword
   };
   let data = {};
-  axios
+  return axios
     .get("https://api.github.com/search/repositories", {
       params: params
     })
-    .then(response => {
-      return response.data.items;
-    })
-    .then(repos => {
-      let count = 0;
-      repos.forEach(repo => {
-        console.log(repo);
-        const { id, name, owner, html_url } = repo;
-        data[id] = {
-          id,
-          name,
-          owner,
-          html_url
-        };
-      });
-      console.log(data);
-    })
-    .catch(e => console.log(e));
-};
+    .then(response => response.data.items);
+}
 
 module.exports = {
   fetchRepositories
